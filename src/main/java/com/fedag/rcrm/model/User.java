26befb5b1034ed16a89299e5e3964ad7ed8b7e7a
Vasiliy Belyavskiy <vasiliyblanco@gmail.com>
2022-08-31 20:14:20 +0300

    Refactoring pom.xml and User

--- src/main/java/com/fedag/rcrm/model/User.java
+++ src/main/java/com/fedag/rcrm/model/User.java
@@ -1,12 +1,16 @@
 package com.fedag.rcrm.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class User {
 
@@ -26,30 +30,6 @@ public abstract class User {
     public User(long id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
