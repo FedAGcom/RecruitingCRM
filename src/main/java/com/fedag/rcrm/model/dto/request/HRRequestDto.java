3678f0b819d40f0dcf0431db2d8bcff53b792021
Polina Babyakina <polinababyakina@yandex.ru>
2022-09-09 09:53:19 +0300

    Add mapper, repository, service, controller, dto for HR

--- src/main/java/com/fedag/rcrm/model/dto/request/HRRequestDto.java
+++ src/main/java/com/fedag/rcrm/model/dto/request/HRRequestDto.java
@@ -1,4 +1,19 @@
 package com.fedag.rcrm.model.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class HRRequestDto {
+    private String firstName;
+    private String lastname;
+    private String login;
+    private char[] password;
+    private List<String> roles;
 }
