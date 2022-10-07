351fdfa09e68f842642ab7744a3d2b7a2a59433b
”Ivan <chicherinegor@gmail.com>
2022-10-07 12:54:16 +0300

    add UserGlobalExceptionHandler class for handling exception in controller methods

--- src/main/java/com/fedag/rcrm/security/security_config/WebSecurityConfig.java
+++ src/main/java/com/fedag/rcrm/security/security_config/WebSecurityConfig.java
@@ -5,26 +5,25 @@ import com.fedag.rcrm.security.RestAuthenticationEntryPoint;
 import com.fedag.rcrm.security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(
          securedEnabled = true,
          jsr250Enabled = true,
         prePostEnabled = true)
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig {
 
     @Autowired
     HrDetailsServiceImpl service;
@@ -71,7 +70,6 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 .logout((logout) -> logout.permitAll());
 
         http.authenticationProvider(authenticationProvider());
-
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
